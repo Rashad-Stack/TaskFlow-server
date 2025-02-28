eb0238ffd48d4e5118c4c87eddbd7ec94fe5a2e2
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-02-28 13:40:39 +0600

    Update CORS settings to remove trailing slash from allowed origin

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://rashad-stack-taskflow.netlify.app/",
+      "https://rashad-stack-taskflow.netlify.app",
     ],
     credentials: true,
   })

5faa17b3b2491cf9158c48c93bf25513f7ec36fa
Rashad-Stack <tex.eng.rashed@gmail.com>
2025-02-28 13:34:42 +0600

    Remove Socket.IO integration and related dependencies from server setup

--- src/server.js
+++ src/server.js
@@ -1,29 +1,11 @@
 const dotenv = require("dotenv");
-const http = require("http");
 const app = require("./app");
 const logger = require("./utils/logger");
 const process = require("process");
-const { Server } = require("socket.io");
 
 dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
-});
-
-io.on("connection", (socket) => {
-  logger.info("New client connected");
-
-  socket.on("disconnect", () => {
-    logger.info("Client disconnected");
-  });
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
